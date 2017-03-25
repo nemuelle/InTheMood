463ddcd8d89b33513be4371a0cb24deca4c646e9
Jamie Yurick <jyurick@ualberta.ca>
2017-03-25 14:26:14 -0600

    Working on ElasticSearch controller addMoods function

--- InTheMood/app/src/main/java/com/example/austin/inthemood/ElasticSearchController.java
+++ InTheMood/app/src/main/java/com/example/austin/inthemood/ElasticSearchController.java
@@ -3,10 +3,15 @@ package com.example.austin.inthemood;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.searchly.jestdroid.DroidClientConfig;
 import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
 
+import java.util.ArrayList;
+
+import io.searchbox.action.Action;
+import io.searchbox.core.DocumentResult;
 import io.searchbox.core.Index;
 
 /**
@@ -15,38 +20,51 @@ import io.searchbox.core.Index;
 
 public class ElasticSearchController {
     private static JestDroidClient client;
+    private String query;
 
-    // TODO we need a function which adds tweets to elastic search
-    public static class AddTweetsTask extends AsyncTask<Mood, Void, Void> {
+    // TODO we need a function which adds moods to elastic search
+    public static class AddMoodsTask extends AsyncTask<Mood, Void, Void> {
+
+
 
         @Override
         protected Void doInBackground(Mood... moods) {
-            //verifySettings();
+            verifySettings();
+
 
             for (Mood mood : moods) {
-                Index index = new Index.Builder(mood).index("testing").type("moods").build();
+
 
                 try {
                     // where is the client?
+                    DocumentResult result = client.execute(new Index.Builder(mood).index("InTheMood").type("Mood").build());
+                    if (result.isSucceeded() == false) {
+                        Log.i("Error", "Elastic search couldn't add mood");
+                    }
+
+
                 }
                 catch (Exception e) {
                     Log.i("Error", "The application failed to build and send the moods");
                 }
-
             }
+
             return null;
         }
     }
 
-    // TODO we need a function which gets tweets from elastic search
-/*    public static class GetMoodsTask extends AsyncTask<String, Void, ArrayList<Moods>> {
+
+    // TODO we need a function which gets moods for a given user from elastic search
+    public static class GetMoodsForUser extends AsyncTask<String, Void, ArrayList<Mood>> {
         @Override
-        protected ArrayList<Mood> doInBackground(String... search_parameters) {
+        protected ArrayList<Mood> doInBackground(String... user) {
             verifySettings();
 
             ArrayList<Mood> moods = new ArrayList<Mood>();
 
                 // TODO Build the query
+
+
 
             try {
                // TODO get the results of the query
@@ -57,7 +75,7 @@ public class ElasticSearchController {
 
             return moods;
         }
-    }*/
+    }
 
 
 
@@ -71,5 +89,6 @@ public class ElasticSearchController {
             factory.setDroidClientConfig(config);
             client = (JestDroidClient) factory.getObject();
         }
+
     }
 }
--- InTheMood/app/src/main/java/com/example/austin/inthemood/MainUser.java
+++ InTheMood/app/src/main/java/com/example/austin/inthemood/MainUser.java
@@ -38,11 +38,15 @@ public class MainUser extends AppCompatActivity {
     private static final String FILENAME = "file.sav";
     public dataControler controller;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
         loadFromFile();
+        Mood test = new Mood();
+        ElasticSearchController.AddMoodsTask addMoods = new ElasticSearchController.AddMoodsTask();
+        addMoods.execute(test);
     }
 
     @Override
