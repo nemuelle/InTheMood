78a2118c1218523478c35721d3f69abcfae52d27
opesto <opreston@ualberta.ca>
2017-03-29 12:20:22 -0600

    fixed a MyFriends activity bug

--- InTheMood/app/src/main/java/com/example/austin/inthemood/MyFriends.java
+++ InTheMood/app/src/main/java/com/example/austin/inthemood/MyFriends.java
@@ -43,6 +43,7 @@ public class MyFriends extends AppCompatActivity {
         //Print to list view. For each followed user, print his name and his most recent mood with mood date
         //just print followeduser name if no moods have been recorded
         myFriendsListView = (ListView) findViewById(R.id.myFriendsListView);
+        followingList = new ArrayList<User>();
         for (int i = 0; i < controller.getCurrentUser().getMyFollowingList().size(); i++){
             followingList.add(controller.searchForUserByName(controller.getCurrentUser().getMyFollowingList().get(i)));
         }
