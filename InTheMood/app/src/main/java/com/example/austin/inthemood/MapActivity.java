0e98088f5df0b943896b0ef6bdf41f54f74adc7e
nemuelle <nemuelle@ualberta.ca>
2017-04-01 17:43:27 -0600

    update

--- InTheMood/app/src/main/java/com/example/austin/inthemood/MapActivity.java
+++ InTheMood/app/src/main/java/com/example/austin/inthemood/MapActivity.java
@@ -1,7 +1,6 @@
 package com.example.austin.inthemood;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
