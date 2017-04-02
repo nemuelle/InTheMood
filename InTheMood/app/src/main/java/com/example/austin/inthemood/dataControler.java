ac413a8f83cb89ecad801bf4b9ee52200b485429
Jamie Yurick <jyurick@ualberta.ca>
2017-04-02 15:55:11 -0600

    Added methods to dataControler to get all users from elastic search and to get the moods within 5km of a location

--- InTheMood/app/src/main/java/com/example/austin/inthemood/dataControler.java
+++ InTheMood/app/src/main/java/com/example/austin/inthemood/dataControler.java
@@ -1,10 +1,12 @@
 package com.example.austin.inthemood;
 
 import android.content.Context;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -19,9 +21,11 @@ import java.util.Date;
 
 public class dataControler {
     private ArrayList<User> userList = new ArrayList<>();
+    private ArrayList<Mood> moodList = new ArrayList<>();
     private int userCount;
     private String currentUserName;
     Context context;
+
 
     /**
      * Instantiates a new dataControler without context. (used for JUnit testing)
@@ -321,7 +325,73 @@ public class dataControler {
             Log.i("Error", "Failed to sync user");
             return false;
         }
+    }
 
+    public String ElasticSearchAddMood(Mood mood) {
+        ElasticSearchController.AddMoodTask addMood = new ElasticSearchController.AddMoodTask();
+        String esID = new String();
+        addMood.execute(mood);
+        try {
+            esID = addMood.get();
+        } catch (Exception e) {
+            Log.i("Error", "Failed to add mood to elastic search");
+        }
+        return esID;
+    }
+
+    public Boolean ElasticSearchSyncMood(Mood mood) {
+        ElasticSearchController.SyncMoodTask syncMood = new ElasticSearchController.SyncMoodTask();
+        Boolean syncSuccess = new Boolean(false);
+        syncMood.execute(mood);
+        try {
+            syncSuccess = syncMood.get();
+        } catch (Exception e) {
+            Log.i("Error", "Failed to sync the mood");
+        }
+        return syncSuccess;
+    }
+
+    public ArrayList<User> ElasticSearchGetAllUsers() {
+        ArrayList<User> users = new ArrayList<>();
+        ElasticSearchController.GetAllUsers getUsers = new ElasticSearchController.GetAllUsers();
+        getUsers.execute("");
+        try {
+            users = getUsers.get();
+        } catch (Exception e) {
+            Log.i("Error", "Failed to get the users");
+        }
+
+        return users;
+    }
+
+    public ArrayList<Mood> getNearMoods(LatLng currentLocation) {
+        ArrayList<Mood> closeMoods = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
+        users = ElasticSearchGetAllUsers();
+        Location fromPoint = new Location("from");
+        fromPoint.setLatitude(currentLocation.latitude);
+        fromPoint.setLongitude(currentLocation.longitude);
+
+        for (int x = 0; x < users.size(); x++) {
+            User user = users.get(x);
+            ArrayList<Mood> usersMoods = user.getMyMoodsList();
+            ArrayList<Mood> sortedMoods = sortMoodsByDate(usersMoods);
+            Mood mostRecentMood = sortedMoods.get(sortedMoods.size()-1);
+            if (mostRecentMood.getLatLng() != null) {
+                LatLng moodLocation = mostRecentMood.getLatLng();
+                Location toPoint = new Location("to");
+                toPoint.setLatitude(moodLocation.latitude);
+                toPoint.setLongitude(moodLocation.longitude);
+                if (toPoint.distanceTo(fromPoint) <= 5000) {
+                    closeMoods.add(mostRecentMood);
+                }
+            }
+
+        }
+
+
+
+        return closeMoods;
     }
 
 
